a95be2ea4073a1187e2840672a61fab78ee48871
dropdeadfred88 <henderson89vm@gmail.com>
2025-11-19 03:22:11 -0600

    Create -hello-world.py

---
+++ -hello-world.py
@@ -0,0 +1,10 @@
+name = input("What's your name? ")
+print("Hey " + name + ", let's do some math!")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print(num1, "+", num2, "=", num1 + num2)
+print(num1, "-", num2, "=", num1 - num2)
+print(num1, "x", num2, "=", num1 * num2)
+print(num1, "/", num2, "=", num1 / num2)
